19215d58ecccebf05bf6e2e4447c5b40c34acf8c
BrandtNorwood <66198245+BrandtNorwood@users.noreply.github.com>
2023-06-17 00:46:53 -0500

    Huge Update to testDataBase.js
    -Slayed the CORS Dragon
    -Added "PUT" functionality
    --Changed 'testTable' to a 'var'

--- testDataBase.js
+++ testDataBase.js
@@ -10,7 +10,7 @@ const http = require('http');
 
 const app = express();
 
-const testTable = [
+var testTable = [
   ["N76NB", "N510AF"],
   ["N446BY", "N988C", "N71TB", "N101BK"],
   ["N602PM", "N113SW", "N6053S", "N706WL", "N701FJ", "N813KS", "N41XP", "N555DS", "N686PC"],
@@ -19,19 +19,50 @@ const testTable = [
   ["N701FC", "N232RJ", "N48LT", "N643RT", "N448BY", "N404CM", "N351LS", "N21DP"]
 ];
 
-// I got this off the internet
-// Handle GET request
-app.get('/api/data', (req, res) => {
-  //f**k CORS (For now)
-  res.set('Access-Control-Allow-Origin', '*');
+// I got most of this off the internet
+
+//slay the god forsaken CORS dragon
+//ALL HAIL CHAT GPT for this solution
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+//this one is for troubleshooting. Just contact the server in the browser and see if its up
+app.get("/", (req,res) => {res.json("Im Awake!" + Date.now()); console.log("Contacted in browser");});
+app.use(express.json());
+
+
+// Handle GET hanger data request
+app.get('/hangerData', (req, res) => {
 
   // Create a JSON object
   const json = {testTable};
 
   // Respond with the JSON object
   res.json(json);
+
   console.log(`Server Contacted - ` + Date.now());
 });
+
+//handles new data being given to the server from the audit table menu
+app.put('/auditData', (req, res) =>{
+
+  var newData = req.body;
+
+  var printData = "";
+
+  for (var i = 0; i < newData.length; i++){
+    printData += ("\n[" + newData[i] + "]");
+  }
+
+  testTable = newData;
+
+  console.log("Server Given Table! - " + printData)
+});
+
 
 
 // Start the server
