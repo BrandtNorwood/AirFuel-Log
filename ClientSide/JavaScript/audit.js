84e1d70db024863d8b047850ad4d07d7895d1b06
BrandtNorwood <66198245+BrandtNorwood@users.noreply.github.com>
2023-06-24 21:19:42 -0500

    major rework
    literally deleted the old file and made a new one
    - Undo functionality
    - SQL functionality

--- ClientSide/JavaScript/audit.js
+++ ClientSide/JavaScript/audit.js
@@ -1,10 +1,9 @@
 //change these two as needed
-const Url = "http://localhost:3000/" //this will be the data base till i get my shit together
+const Url = "http://localhost:3000/" //this is the server root url (extensions will be added by other scripts)
 const hangers = ["Ramp","Alpha","Bravo","Charlie","Delta","Echo"]; //when the jet center gets more hangers change this line
 
 
-
-//This is for populating the Hanger selection menu in the Audit page
+//This is for detecting the enter key and handling it properly
 function textBoxListener(){
     var textBox = document.getElementById("tailInput");
 
@@ -14,114 +13,16 @@ function textBoxListener(){
 }
 
 //Populates the Hanger Select menu from the hangers constant
-function auditMenu(){
+function hangerSelectMenu(){
     var hangerMenu = document.getElementById("hangerSelect");
 
     for (var i=1; i < hangers.length; i++){
         hangerMenu.options[hangerMenu.options.length] = new Option(hangers[i]);
     }
-
-    hangerMenu.onchange = function(){auditTable();}
+    hangerMenu.onchange = function(){clearChanges();}
 }
 
-//get tail numbers that have been green flaged
-function getGreenList(){
-    currentHTMLGreen = document.getElementsByClassName("green");
-    var currentGreen = new Array();
-
-    for(element of currentHTMLGreen){
-        currentGreen.push(element.innerText);
-    }
-
-    return currentGreen;
-}
-
-//gets tail numbers that have been yellow flaged
-function getYellowList(){
-    currentHTMLYellow = document.getElementsByClassName("yellow");
-    var currentYellow = new Array();
-
-    for(element of currentHTMLYellow){
-        currentYellow.push(element.innerText);
-    }
-
-    return currentYellow;
-}
-
-//get all tail numbers in the left column
-//this works in a stupid and rediculous way but... It does work!
-function getAllList(){
-    currentHTMLList = document.getElementsByClassName("list");
-    var currentList = new Array();
-
-    for(element of currentHTMLList){
-        currentList.push(element.innerText);
-    }
-
-    return currentList;
-}
-
-// Example POST method implementation: (stolen from ChatGPT)
-async function postData(url = "", sendData = {}) {
-    await fetch(url, {
-        method: "POST", 
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(sendData)
-    });
-}
-
-function resetTable(){
-    var yellowList = new Array();
-    var greenList = new Array();
-
-    auditTable();
-}
-
-
-function greenListAll(){
-    var tableList = getAllList();
-    var yellowList = getYellowList();
-    var greenList = new Array();
-
-    for (element of tableList){
-        greenList.push(element);
-    }
-
-    auditTable(greenList,yellowList);
-}
-
-//adds aircraft to the left column (yellow or green flaged)
-function addAuditList(){
-    //retrives green and yellow lists
-    var greenList = getGreenList();
-    var yellowList = getYellowList();
-    var tableList = getAllList();
-
-    //defines and reads text box for future use
-    var inputTail = document.getElementById("tailInput");
-    var inputTailValue = document.getElementById("tailInput").value.trim();
-
-    if (inputTailValue != "N" && inputTailValue != ""){
-
-        var isOnTable = false;
-        for(element of greenList){if (inputTailValue.toUpperCase() == element){ isOnTable = true;}}
-        for(element of tableList){if (inputTailValue.toUpperCase() == element){ isOnTable = true;}}
-
-        if (!isOnTable){
-            yellowList.push(inputTailValue.toUpperCase());
-        }
-        else {
-            //adds to greenlist
-            greenList.push(inputTailValue.toUpperCase());
-        }
-
-        //clears the text box
-        inputTail.value = "N";
-
-        auditTable(greenList, yellowList);
-    }
-}
-
+//this is for highlighting tails on click
 function setInput(tailNumber){
     var inputTail = document.getElementById("tailInput");
 
@@ -130,7 +31,14 @@ function setInput(tailNumber){
     addAuditList();
 }
 
-function sendHangerList(){
+//defined these here so they have persistance
+var submittedList = new Array();
+var originalList = new Array();
+
+//querys the server for a hanger table
+function retreiveOG(){
+    var hangerSelect = document.getElementById("hangerSelect");
+
     fetch(Url+"hangerData")
     .then(function(response){
         if(response.ok){
@@ -139,8 +47,7 @@ function sendHangerList(){
         throw new Error('"fetch" threw Json Formating error - ' + response.status + ' ' + response.statusText);
     })
     .then(function(data){
-        //seperate the table from the JSON object
-        hangerSelect = document.getElementById("hangerSelect");
+        tableData = data.sqlTable;
 
         var selectedHanger = 0;
 
@@ -150,97 +57,136 @@ function sendHangerList(){
             }
         }
 
-        var inputHanger= getGreenList().concat(getYellowList());
+        originalList = tableData[selectedHanger];
 
-        var sendData = [selectedHanger,inputHanger];
-
-        postData(Url + "auditData", sendData)
-        .then(() => {
-            resetTable();
-        });
-
-    })
-      .catch(function(error) {
-        // Handle any errors that occurred during the request
-        console.log(error)
-      });
+        buildTable();
+    });
 }
 
+//adds item in submit box into submit list while blocking duplicate and empty entries
+function addAuditList(){
+    var inputTail = document.getElementById("tailInput");
+    var inputTailValue = document.getElementById("tailInput").value.trim();
 
-//Defines the Left side audit table
-function auditTable(greenList,yellowList){
-    fetch(Url+"hangerData")
-    .then(function(response){
-        if(response.ok){
-            return response.json();
+    inputTailValue = inputTailValue.toUpperCase();
+
+    if (inputTailValue != "N" && inputTailValue != ""){
+        var alreadyEntered = false;
+
+        for(element of submittedList){
+            if (element == inputTailValue){alreadyEntered = true;}
         }
-        throw new Error('"fetch" threw Json Formating error - ' + response.status + ' ' + response.statusText);
+
+        if(!alreadyEntered){
+            submittedList.push(inputTailValue);
+        }
+    }
+    inputTail.value ="N";
+
+    buildTable();
+}
+
+//returns items to be added, removed, and kept
+function compareLists(){
+    var addList = new Array();
+    var removeList = new Array();
+    var confirmedList = new Array();
+
+    for (element of originalList){
+        if (!submittedList.includes(element)){
+            removeList.push(element);
+        } else {
+            confirmedList.push(element);
+        }
+    }
+
+    for (element of submittedList){
+        if (!originalList.includes(element)){
+            addList.push(element);
+        }
+    }
+
+    return {addList,removeList,confirmedList};
+}
+
+//handles undo button (which is super easy because im a genius)
+function undoSubmit(){
+    submittedList.pop();
+    buildTable();
+}
+
+//resets table to what is currently in the hanger
+function clearChanges(){
+    submittedList = new Array();
+    retreiveOG();
+}
+
+//based on the name you should hopefully be able to guess what this does
+function buildTable(){
+    //this spits out 3 arrays that corospond to color
+    var compareThingamajig = compareLists();
+        var addList = compareThingamajig.addList;
+        var removeList = compareThingamajig.removeList;
+        var confirmedList = compareThingamajig.confirmedList;
+
+    //this combine the
+    var displayList = [...new Set(originalList.concat(submittedList))];
+    outputTable = document.getElementById("auditTable");
+
+    outputTable.replaceChildren();
+
+    displayList.sort();
+
+    //create the html objects for the table
+    var table = document.createElement('ol');
+
+    //adding the elements to te table
+    for (element of displayList){
+        var cell = document.createElement('ul');
+        cell.appendChild(document.createTextNode(element));
+        cell.setAttribute("onclick","setInput('"+element+"')");
+
+        //this is for setting colors
+        if (submittedList.length > 0){
+            if (addList.includes(element)){cell.setAttribute("class","yellow");}
+            if (removeList.includes(element)){cell.setAttribute("class","red");}
+            if (confirmedList.includes(element)){cell.setAttribute("class","green");}
+        }
+
+        table.appendChild(cell);
+    }
+
+    //writing the table to the html doc
+    outputTable.appendChild(table);
+}
+
+//Sends the changes off to the NodeJS server
+function submitChanges(){
+    var hangerSelect = document.getElementById("hangerSelect");
+
+    //this spits out the arrays that are going to be passed to the server
+    var compareThingamajig = compareLists();
+        var addList = compareThingamajig.addList;
+        var removeList = compareThingamajig.removeList;
+
+    var selectedHanger = 0;
+
+    for (var i=0; i < hangers.length; i++){
+        if (hangers[i] == hangerSelect.value){
+            selectedHanger = i;
+        }
+    }
+
+    var sendData = [selectedHanger, addList,removeList];
+
+    console.log(sendData);
+
+    fetch(Url + "auditData", {
+        method: "POST", 
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify([selectedHanger,addList,removeList])
     })
-    .then(function(data){
-        //seperate the table from the JSON object
-        tableData = data.sqlTable;
-        hangerSelect = document.getElementById("hangerSelect");
-        outputTable = document.getElementById("auditTable");
-
-        outputTable.replaceChildren();
-
-        var selectedHanger = 0;
-
-        for (var i=0; i < hangers.length; i++){
-            if (hangers[i] == hangerSelect.value){
-                selectedHanger = i;
-            }
-        }
-
-        //create the html objects for the table
-        var table = document.createElement('ol');
-
-        if(yellowList != undefined){
-            for (var i=0; i < yellowList.length; i++){
-                tableData[selectedHanger].push(yellowList[i]);
-            }
-        }
-
-        tableData[selectedHanger].sort();
-
-        tableData[selectedHanger].forEach(element => {
-            var cell = document.createElement('ul');
-            cell.appendChild(document.createTextNode(element))
-            cell.setAttribute("onclick","setInput('"+element+"')");
-
-            if (greenList == undefined && yellowList == undefined){cell.setAttribute("class","blue")}
-
-            if(greenList != undefined){
-                for (var i=0; i < greenList.length; i++){
-                    if (greenList[i] == element){
-                        cell.setAttribute("class","green");
-                    }
-                }
-            }
-
-            if(yellowList != undefined){
-                for (var i=0; i < yellowList.length; i++){
-                    if (yellowList[i] == element){
-                        cell.setAttribute("class","yellow");
-                    }
-                }
-            }
-
-            if (cell.getAttribute("class") != null){
-                cell.setAttribute("class",cell.getAttribute("class") + " list")
-            }else {
-                cell.setAttribute("class","list");
-            }
-
-            table.appendChild(cell);
-        });
-    
-        table.setAttribute("id","list")
-        outputTable.appendChild(table);     
-
-      })
-      .catch(function(error) {
-        // Handle any errors that occurred during the request
-        console.log(error)
-      });
+    .then(() => {
+        clearChanges();
+    });
 }
